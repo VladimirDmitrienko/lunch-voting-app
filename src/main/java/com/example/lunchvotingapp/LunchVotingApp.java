816364101dcc42a90facfd056294308803495068
Vladimir Dmitrienko <dmitrienkovladimir97@gmail.com>
2023-12-11 20:10:09 +0100

    Initial commit.

---
+++ src/main/java/com/example/lunchvotingapp/LunchVotingApp.java
@@ -0,0 +1,12 @@
+package com.example.lunchvotingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchVotingApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunchVotingApp.class, args);
+    }
+}
